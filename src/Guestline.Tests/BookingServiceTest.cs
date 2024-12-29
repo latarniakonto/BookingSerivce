5e59e82b1595959fd6f11acbe922e001c414628d
latarniakonto <wisniewskikq27@gmail.com>
2024-12-29 16:18:07 +0100

    Test uncrecognized room type

--- src/Guestline.Tests/BookingServiceTest.cs
+++ src/Guestline.Tests/BookingServiceTest.cs
@@ -104,5 +104,33 @@ public class BookServiceTest
         // Act & Assert
         var exception = Assert.Throws<InvalidOperationException>(() => bookingService.GetRoomAvailability("H1", "20240901-20240903", "SGL"));
     }
+
+    [Fact]
+    public void Check_Hotel_Occupancy_Unrecognized_Room_Type()
+    {
+        // Arrange
+        var fixture = new Fixture();
+        fixture.Customize<Room>(r => r
+                .With(r1 => r1.RoomId, fixture.Create<string>())
+                .With(r1 => r1.RoomType, "SGL"));
+
+        fixture.Customize<RoomType>(rt => rt
+                .With(rt1 => rt1.Code, "SGL")
+                .With(rt1 => rt1.Description, fixture.Create<string>()));
+
+        fixture.Customize<Hotel>(h => h
+            .With(h1 => h1.Id, "H1").With(h1 => h1.Name, "Test1")
+            .With(h1 => h1.RoomTypes, fixture.CreateMany<RoomType>(1).ToList())
+            .With(h1 => h1.Rooms, fixture.CreateMany<Room>(3).ToList()));
+
+        var bookingService = fixture.Build<BookingService>()
+            .With(bs => bs.bookings, new List<Booking>())
+            .With(bs => bs.hotels, fixture.CreateMany<Hotel>(1).ToList())
+            .Create();
+        // Act
+        int availability = bookingService.GetRoomAvailability("H1", "20240901-20240903", "DBL");
+        // Assert
+        Assert.Equal(0, availability);
+    }
 }
 
